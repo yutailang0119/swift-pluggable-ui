c5fd167769b708ef38196592c509e280a6fc7d6d
yutailang0119 <yutailang0119@gmail.com>
2024-08-02 00:06:10 +0900

    Fix to set platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "swift-pluggable-ui",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8),
+    ],
     products: [
         .library(
             name: "SwiftPluggableUI",
