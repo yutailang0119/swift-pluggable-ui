360d4ea3fde0c5d7cda68b0a1887b4e938708fce
yutailang0119 <yutailang0119@gmail.com>
2024-08-06 03:02:56 +0900

    Fix Example to cleanup

--- Example/Package.swift
+++ Example/Package.swift
@@ -5,20 +5,5 @@ import PackageDescription
 
 let package = Package(
     name: "Example",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Example",
-            targets: ["Example"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Example"),
-        .testTarget(
-            name: "ExampleTests",
-            dependencies: ["Example"]
-        ),
-    ]
+    targets: []
 )
