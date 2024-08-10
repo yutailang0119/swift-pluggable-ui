37bff15d0f3fb9e49623bbfb573fcf7aa91fe8a5
yutailang0119 <yutailang0119@gmail.com>
2024-08-11 03:05:32 +0900

    Add PluggableUIMacro as ExtensionMacro

---
+++ Sources/SwiftPluggableUIMacros/PluggableUIMacro.swift
@@ -0,0 +1,15 @@
+import SwiftDiagnostics
+import SwiftSyntax
+import SwiftSyntaxMacros
+
+public struct PluggableUIMacro: ExtensionMacro {
+    public static func expansion(
+        of node: SwiftSyntax.AttributeSyntax,
+        attachedTo declaration: some SwiftSyntax.DeclGroupSyntax,
+        providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol,
+        conformingTo protocols: [SwiftSyntax.TypeSyntax],
+        in context: some SwiftSyntaxMacros.MacroExpansionContext
+    ) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
+        fatalError()
+    }
+}
