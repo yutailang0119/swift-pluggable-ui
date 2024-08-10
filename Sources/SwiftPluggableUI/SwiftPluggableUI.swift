e5e39640cc329dbda92e921720139d50a412ba01
yutailang0119 <yutailang0119@gmail.com>
2024-08-11 03:55:34 +0900

    Add PluggableUI as Attached macro

--- Sources/SwiftPluggableUI/SwiftPluggableUI.swift
+++ Sources/SwiftPluggableUI/SwiftPluggableUI.swift
@@ -1,2 +1,8 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+@attached(
+    extension,
+    conformances: SwiftUI.View,
+    names: named(body)
+)
+public macro PluggableUI() = #externalMacro(module: "SwiftPluggableUIMacros", type: "PluggableUIMacro")
