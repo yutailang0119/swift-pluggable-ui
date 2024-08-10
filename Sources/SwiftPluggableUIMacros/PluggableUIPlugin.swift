9f4f790115fcff1707cc44ea8e55ed49af311c9a
yutailang0119 <yutailang0119@gmail.com>
2024-08-11 03:07:24 +0900

    Add PluggableUIPlugin

---
+++ Sources/SwiftPluggableUIMacros/PluggableUIPlugin.swift
@@ -0,0 +1,9 @@
+import SwiftCompilerPlugin
+import SwiftSyntaxMacros
+
+@main
+struct PluggableUIPlugin: CompilerPlugin {
+    let providingMacros: [Macro.Type] = [
+        PluggableUIMacro.self,
+    ]
+}
