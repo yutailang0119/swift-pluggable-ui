110547078125107b03fa475aa8b988d5d8e31202
yutailang0119 <yutailang0119@gmail.com>
2024-10-19 22:54:40 +0900

    Fix tests to use Testing

--- Tests/SwiftPluggableUITests/SwiftPluggableUITests.swift
+++ Tests/SwiftPluggableUITests/SwiftPluggableUITests.swift
@@ -1,13 +1,9 @@
-import XCTest
+import Testing
 
 @testable import SwiftPluggableUI
 
-final class SwiftPluggableUITests: XCTestCase {
-  func testExample() throws {
-    // XCTest Documentation
-    // https://developer.apple.com/documentation/xctest
-
-    // Defining Test Cases and Test Methods
-    // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-  }
+@Suite
+struct SwiftPluggableUITests {
+  @Test
+  func example() throws {}
 }
