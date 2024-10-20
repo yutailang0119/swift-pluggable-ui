01f50f23394ee521c9589e4014489386d094129d
yutailang0119 <yutailang0119@gmail.com>
2024-10-20 18:21:38 +0900

    Fix SwiftPluggableUITests to depend on SwiftPluggableUIMacros

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     ),
     .testTarget(
       name: "SwiftPluggableUITests",
-      dependencies: ["SwiftPluggableUI"]
+      dependencies: ["SwiftPluggableUIMacros"]
     ),
   ]
 )
