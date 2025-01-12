2a53eabc02aa3501145c24f63062eba841cb5d0f
yutailang0119 <yutailang0119@gmail.com>
2025-01-12 16:46:00 +0900

    Fix PluggableUIMacro output to cleanup

--- Sources/SwiftPluggableUIMacros/PluggableUIMacro.swift
+++ Sources/SwiftPluggableUIMacros/PluggableUIMacro.swift
@@ -24,125 +24,123 @@ public struct PluggableUIMacro: ExtensionMacro {
       throw error
     }
 
-    let syntax = ExtensionDeclSyntax(
-      extendedType: IdentifierTypeSyntax(name: typeName),
-      inheritanceClause: protocols.isEmpty
-        ? nil
-        : InheritanceClauseSyntax {
-          protocols.map {
-            InheritedTypeSyntax(type: $0)
-          }
-        },
-      memberBlock: MemberBlockSyntax {
-        VariableDeclSyntax(
-          modifiers: [DeclModifierSyntax(name: .keyword(.public))],
-          bindingSpecifier: .keyword(.var),
-          bindings: PatternBindingListSyntax {
-            PatternBindingSyntax(
-              pattern: PatternSyntax(
-                IdentifierPatternSyntax(
-                  identifier: .identifier("body")
-                )
-              ),
-              typeAnnotation: TypeAnnotationSyntax(
-                type: SomeOrAnyTypeSyntax(
-                  someOrAnySpecifier: .keyword(.some),
-                  constraint: IdentifierTypeSyntax(
-                    name: .identifier("View")
+    return [
+      ExtensionDeclSyntax(
+        extendedType: IdentifierTypeSyntax(name: typeName),
+        inheritanceClause: protocols.isEmpty
+          ? nil
+          : InheritanceClauseSyntax {
+            protocols.map {
+              InheritedTypeSyntax(type: $0)
+            }
+          },
+        memberBlock: MemberBlockSyntax {
+          VariableDeclSyntax(
+            modifiers: [DeclModifierSyntax(name: .keyword(.public))],
+            bindingSpecifier: .keyword(.var),
+            bindings: PatternBindingListSyntax {
+              PatternBindingSyntax(
+                pattern: PatternSyntax(
+                  IdentifierPatternSyntax(
+                    identifier: .identifier("body")
                   )
-                )
-              ),
-              accessorBlock: AccessorBlockSyntax(
-                accessors: .getter(
-                  CodeBlockItemListSyntax {
-                    CodeBlockItemSyntax(
-                      item: .expr(
-                        ExprSyntax(
-                          IfExprSyntax(
-                            conditions: ConditionElementListSyntax {
-                              OptionalBindingConditionSyntax(
-                                bindingSpecifier: .keyword(.let),
-                                pattern: PatternSyntax(
-                                  IdentifierPatternSyntax(
-                                    identifier: .identifier("plugin")
-                                  )
-                                ),
-                                initializer: InitializerClauseSyntax(
-                                  value: AsExprSyntax(
-                                    expression: DeclReferenceExprSyntax(
-                                      baseName: .keyword(.self)
-                                    ),
-                                    questionOrExclamationMark: .postfixQuestionMarkToken(),
-                                    type: SomeOrAnyTypeSyntax(
-                                      someOrAnySpecifier: .keyword(.any),
-                                      constraint: IdentifierTypeSyntax(
-                                        name: .identifier("PluginUI")
+                ),
+                typeAnnotation: TypeAnnotationSyntax(
+                  type: SomeOrAnyTypeSyntax(
+                    someOrAnySpecifier: .keyword(.some),
+                    constraint: IdentifierTypeSyntax(
+                      name: .identifier("View")
+                    )
+                  )
+                ),
+                accessorBlock: AccessorBlockSyntax(
+                  accessors: .getter(
+                    CodeBlockItemListSyntax {
+                      CodeBlockItemSyntax(
+                        item: .expr(
+                          ExprSyntax(
+                            IfExprSyntax(
+                              conditions: ConditionElementListSyntax {
+                                OptionalBindingConditionSyntax(
+                                  bindingSpecifier: .keyword(.let),
+                                  pattern: PatternSyntax(
+                                    IdentifierPatternSyntax(
+                                      identifier: .identifier("plugin")
+                                    )
+                                  ),
+                                  initializer: InitializerClauseSyntax(
+                                    value: AsExprSyntax(
+                                      expression: DeclReferenceExprSyntax(
+                                        baseName: .keyword(.self)
+                                      ),
+                                      questionOrExclamationMark: .postfixQuestionMarkToken(),
+                                      type: SomeOrAnyTypeSyntax(
+                                        someOrAnySpecifier: .keyword(.any),
+                                        constraint: IdentifierTypeSyntax(
+                                          name: .identifier("PluginUI")
+                                        )
                                       )
                                     )
                                   )
                                 )
-                              )
-                            },
-                            body: CodeBlockSyntax(
-                              statements: CodeBlockItemListSyntax {
-                                CodeBlockItemSyntax(
-                                  item: .expr(
-                                    ExprSyntax(
-                                      FunctionCallExprSyntax(
-                                        calledExpression: DeclReferenceExprSyntax(
-                                          baseName: .identifier("AnyView")
-                                        ),
-                                        leftParen: .leftParenToken(),
-                                        arguments: LabeledExprListSyntax {
-                                          LabeledExprSyntax(
-                                            expression: MemberAccessExprSyntax(
-                                              base: DeclReferenceExprSyntax(
-                                                baseName: .identifier("plugin")
-                                              ),
-                                              declName: DeclReferenceExprSyntax(
-                                                baseName: .identifier("pluginBody")
-                                              )
-                                            )
-                                          )
-                                        },
-                                        rightParen: .rightParenToken()
-                                      )
-                                    )
-                                  )
-                                )
-                              }
-                            ),
-                            elseKeyword: .keyword(.else),
-                            elseBody: .codeBlock(
-                              CodeBlockSyntax(
+                              },
+                              body: CodeBlockSyntax(
                                 statements: CodeBlockItemListSyntax {
                                   CodeBlockItemSyntax(
                                     item: .expr(
                                       ExprSyntax(
-                                        DeclReferenceExprSyntax(
-                                          baseName: .identifier("defaultBody")
+                                        FunctionCallExprSyntax(
+                                          calledExpression: DeclReferenceExprSyntax(
+                                            baseName: .identifier("AnyView")
+                                          ),
+                                          leftParen: .leftParenToken(),
+                                          arguments: LabeledExprListSyntax {
+                                            LabeledExprSyntax(
+                                              expression: MemberAccessExprSyntax(
+                                                base: DeclReferenceExprSyntax(
+                                                  baseName: .identifier("plugin")
+                                                ),
+                                                declName: DeclReferenceExprSyntax(
+                                                  baseName: .identifier("pluginBody")
+                                                )
+                                              )
+                                            )
+                                          },
+                                          rightParen: .rightParenToken()
                                         )
                                       )
                                     )
                                   )
                                 }
+                              ),
+                              elseKeyword: .keyword(.else),
+                              elseBody: .codeBlock(
+                                CodeBlockSyntax(
+                                  statements: CodeBlockItemListSyntax {
+                                    CodeBlockItemSyntax(
+                                      item: .expr(
+                                        ExprSyntax(
+                                          DeclReferenceExprSyntax(
+                                            baseName: .identifier("defaultBody")
+                                          )
+                                        )
+                                      )
+                                    )
+                                  }
+                                )
                               )
                             )
                           )
                         )
                       )
-                    )
-                  }
+                    }
+                  )
                 )
               )
-            )
-          }
-        )
-      }
-    )
-
-    return [
-      syntax
+            }
+          )
+        }
+      )
     ]
   }
 }
